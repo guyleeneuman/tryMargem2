b3d08df880775efed70b97ea5d725246d48d754e
guyleeneuman <guyleeneuman@gmail.com>
2026-07-27 09:10:57 +0300

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -8,16 +8,16 @@ push
 }
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
-
 const firebaseConfig = {
   apiKey: "AIzaSyB8S57Iqfi0p_1JgPlz2NT3upakyo-JYUc",
   authDomain: "margemhot.firebaseapp.com",
-  projectId: "margemhot",
+databaseURL:
+"https://margemhot-default-rtdb.europe-west1.firebasedatabase.app",
+ projectId: "margemhot",
   storageBucket: "margemhot.firebasestorage.app",
   messagingSenderId: "798968080375",
   appId: "1:798968080375:web:7e325f48fc16854bdb70f2"
 };
-
 
 const app = initializeApp(firebaseConfig);
 
